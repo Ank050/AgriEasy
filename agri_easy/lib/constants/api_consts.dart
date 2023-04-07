ce61617a435f9784f0fc3046a0e4ebcfa432d9fd
dhanush159 <dhanush.ai21@bmsce.ac.in>
2023-04-07 23:57:12 +0530

    api key update

--- agri_easy/lib/constants/api_consts.dart
+++ agri_easy/lib/constants/api_consts.dart
@@ -1,4 +1,4 @@
 // ignore: non_constant_identifier_names
 String BASE_URL = "https://api.openai.com/v1";
 // ignore: non_constant_identifier_names
-String API_KEY = "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";
+String API_KEY = "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";
